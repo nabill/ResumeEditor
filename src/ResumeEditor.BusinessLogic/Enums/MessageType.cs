6575572802fc8f7d8a7e9f80d31fde0e3796f4d5
Nabil Benkhemou <nabil.benkhemou@gmail.com>
2023-03-11 20:24:24 +0100

    Business logic class library project added

---
+++ src/ResumeEditor.BusinessLogic/Enums/MessageType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResumeEditor.BusinessLogic.Enums
+{
+    public enum MessageType
+    {
+        Title,
+        Normal,
+        Highlight,
+        Success,
+        Warning,
+        Error
+    }
+}
