b1062805c0e4a9a365276528ea796643784a2ba7
Nabil Benkhemou <nabil.benkhemou@gmail.com>
2023-03-12 04:57:49 +0100

    update

--- src/ResumeEditor.Console/Program.cs
+++ src/ResumeEditor.Console/Program.cs
@@ -4,25 +4,9 @@ using ResumeEditor.Console.Helpers;
 
 Menus menus = new ();
 
-
 bool showMenu = true;
 
 Objects.resume = new Resume();
-Objects.resume.Experiences.Add(new ResumeEditor.BusinessLogic.Models.Experience()
-{
-    Title = ".Net Developper",
-    Company = "Computershare",
-    Start = "01/2023",
-    End = "Today"
-});
-Objects.resume.Experiences.Add(new ResumeEditor.BusinessLogic.Models.Experience()
-{
-    Title = ".Net Developper",
-    Company = "Computershare",
-    Start = "01/2023",
-    End = "Today"
-});
-// TODO: Get resume data from source file if any
 
 
 while (showMenu)
